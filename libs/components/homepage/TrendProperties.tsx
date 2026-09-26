95a1e356772e37df146917bef31b6098ed721fcb
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-09-26 15:53:51 +0900

    feat: develop propety like process like within PropertyList screen component

--- libs/components/homepage/TrendProperties.tsx
+++ libs/components/homepage/TrendProperties.tsx
@@ -49,7 +49,7 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 
 			await likeTargetProperty({
 				variables: {
-					input:  id ,
+					input: id,
 				},
 			});
 
@@ -88,7 +88,7 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 								{trendProperties.map((property: Property) => {
 									return (
 										<SwiperSlide key={property._id} className={'trend-property-slide'}>
-											<TrendPropertyCard property={property} likePropertyHandler={likePropertyHandler}/>
+											<TrendPropertyCard property={property} likePropertyHandler={likePropertyHandler} />
 										</SwiperSlide>
 									);
 								})}
